1941fa712b57120f8f61e76744c6bf6ff2827cc6
asfi_rahman <asfirahman@iut-dhaka.edu>
2022-12-11 14:29:14 +0600

    2nd commit

---
+++ src/components/reserve/Reserve.jsx
@@ -0,0 +1,35 @@
+import {faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useFetch from "../../hooks/useFetch";
+import "./reserve.css"
+
+const Reserve = ({setOpen, hotelId}) => {
+
+    const { data, loading, error } = useFetch(`hotels/room/${hotelId}`)
+
+    return (
+        <div className="reserve">
+            <div className="rContainer">
+                <FontAwesomeIcon 
+                icon={faCircleXmark} 
+                className="rclose" 
+                onClick={() => setOpen(false)}
+                />
+                <span>Select your rooms:</span>
+                {data.map((item) => (
+                    <div className="rItem">
+                        <div className="rItemInfo">
+                           <div className="rTitle">{item.title}</div>
+                           <div className="rDesc">{item.desc}</div> 
+                           <div className="rMax">
+                            Max people: <b>{item.maxPeople}</b>
+                            </div>  
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Reserve;
